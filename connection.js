9f5fc1435602c72a58cf8aa597c30b803d766700
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-12 15:54:51 +0000

    Connected to mongoose, created connection folder and required mongoose

--- connection.js
+++ connection.js
@@ -1,26 +1,61 @@
 const mongoose = require('mongoose');
 
-// ES6 Promises
-mongoose.Promise = global.Promise;
+//Connect to mongodb 
+mongoose.connect('mongodb://localhost/testaroo');
 
-// Connect to db before tests run
-before(function(done){
+mongoose.connection.once('open', function(){
+   console.log("Connection has been made! yay!"); 
+}).on('error', function(error){
+    console.log("connection error:", error);
+})
 
-    // Connect to mongodb
-    mongoose.connect('mongodb://localhost/testaroo');
-    mongoose.connection.once('open', function(){
-        console.log('Connection has been made, now make fireworks...');
-        done();
-    }).on('error', function(error){
-        console.log('Connection error:', error);
-    });
 
-});
 
-// Drop the characters collection before each test
-beforeEach(function(done){
-    // Drop the collection
-    mongoose.connection.collections.mariochars.drop(function(){
-        done();
-    });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// // ES6 Promises
+// mongoose.Promise = global.Promise;
+
+// // Connect to db before tests run
+// before(function(done){
+
+//     // Connect to mongodb
+//     mongoose.connect('mongodb://localhost/testaroo');
+//     mongoose.connection.once('open', function(){
+//         console.log('Connection has been made, now make fireworks...');
+//         done();
+//     }).on('error', function(error){
+//         console.log('Connection error:', error);
+//     });
+
+// });
+
+// // Drop the characters collection before each test
+// beforeEach(function(done){
+//     // Drop the collection
+//     mongoose.connection.collections.mariochars.drop(function(){
+//         done();
+//     });
+// });
