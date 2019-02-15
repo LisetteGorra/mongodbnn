91f69ce0dfddbdd9422413fb160ae98fbfd72844
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-15 22:24:01 +0000

    Created new author with nested data and test

--- nesting_test.js
+++ nesting_test.js
@@ -2,53 +2,108 @@ const assert = require('assert');
 const mongoose = require('mongoose');
 const Author = require('../models/authors');
 
-// Describe our tests
+//Describe our test 
+
 describe('Nesting records', function(){
-
-    beforeEach(function(done){
-        // Drop the collection
-        mongoose.connection.collections.authors.drop(function(){
-            done();
-        });
-    });
-
-    // Create tests
-    it('Creates an author with sub-documents', function(done){
-
-        var pat = new Author({
+    
+    //Create tests
+    it('Create an author with sub-documents', function(done){
+        
+        var pat = new Author ({
             name: 'Patrick Rothfuss',
             books: [{title: 'Name of the Wind', pages: 400}]
         });
-
+        
         pat.save().then(function(){
-            Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
-                assert(record.books.length === 1);
-                done();
-            });
+        Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
+           assert(record.books.length === 1);
+        done();
+            
+            });    
         });
-
     });
-
-    it('Adds a book to an author', function(done){
-
-        var pat = new Author({
-            name: 'Patrick Rothfuss',
-            books: [{title: 'Name of the Wind', pages: 400}]
-        });
-
-        pat.save().then(function(){
-            Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
-                // add a book to the books collection
-                record.books.push({title: "Wise Man's Fear", pages: 500});
-                record.save().then(function(){
-                    Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
-                        assert(record.books.length === 2);
-                        done();
-                    });
-                });
-            });
-        });
-
-    });
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const assert = require('assert');
+// const mongoose = require('mongoose');
+// const Author = require('../models/authors');
+
+// // Describe our tests
+// describe('Nesting records', function(){
+
+//     beforeEach(function(done){
+//         // Drop the collection
+//         mongoose.connection.collections.authors.drop(function(){
+//             done();
+//         });
+//     });
+
+//     // Create tests
+//     it('Creates an author with sub-documents', function(done){
+
+//         var pat = new Author({
+//             name: 'Patrick Rothfuss',
+//             books: [{title: 'Name of the Wind', pages: 400}]
+//         });
+
+//         pat.save().then(function(){
+//             Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
+//                 assert(record.books.length === 1);
+//                 done();
+//             });
+//         });
+
+//     });
+
+//     it('Adds a book to an author', function(done){
+
+//         var pat = new Author({
+//             name: 'Patrick Rothfuss',
+//             books: [{title: 'Name of the Wind', pages: 400}]
+//         });
+
+//         pat.save().then(function(){
+//             Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
+//                 // add a book to the books collection
+//                 record.books.push({title: "Wise Man's Fear", pages: 500});
+//                 record.save().then(function(){
+//                     Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
+//                         assert(record.books.length === 2);
+//                         done();
+//                     });
+//                 });
+//             });
+//         });
+
+//     });
+
+// });
