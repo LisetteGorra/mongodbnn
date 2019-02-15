1bce856512558547cd6a5d88f1b97db20bcc7005
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-15 22:36:35 +0000

    Nested sub documents

--- nesting_test.js
+++ nesting_test.js
@@ -6,6 +6,13 @@ const Author = require('../models/authors');
 
 describe('Nesting records', function(){
     
+    beforeEach(function(done){
+        mongoose.connection.collections.authors.drop(function(){
+            done(;)
+        });
+        
+    });
+    
     //Create tests
     it('Create an author with sub-documents', function(done){
         
@@ -21,6 +28,29 @@ describe('Nesting records', function(){
             
             });    
         });
+    });
+    
+    it('Adds a book to an author', function(done){
+        
+        var pat = new Author ({
+            name: 'Patrick Rothfuss',
+            books: [{title: 'Name of the Wind', pages: 400}]
+            
+        });
+        
+        pat.save().then(function(){
+           Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
+               //add a book to the books collection/array 
+               record.books.push ({title: "Wise Man's Fear", pages: 500});
+               record.save().then(function(){
+                  Author.findOne({name:'Patrick Rothfuss'}).then(function(result){
+                     assert(result.books.length === 2);
+                     done();
+                  }); 
+               });
+           }); 
+        });
+        
     });
 });
 
